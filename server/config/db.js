bbbde17a1f181a7f036a81724d032712c3ec4d1e
quiaro <quiaro@gmail.com>
2016-09-21 16:48:39 -0600

    Connect to remote database

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,22 @@
 var knex = require('knex')({
   client: 'mysql',
+  // connection: {
+  //   host     : '127.0.0.1',
+  //   user     : 'davidq',
+  //   password : 'miccheck1212',
+  //   database : 'testdb',
+  //   charset  : 'utf8'
+  // }
+
+  // Remote connection
   connection: {
-    host     : '127.0.0.1',
-    user     : 'davidq',
+    host     : 'secure164.inmotionhosting.com',
+    user     : 'procel5_e924',
     password : 'miccheck1212',
-    database : 'testdb',
+    database : 'procel5_e924',
     charset  : 'utf8'
   }
+
 });
 
 module.exports = require('bookshelf')(knex);
